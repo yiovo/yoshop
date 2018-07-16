723912886a04e07003a87694e78a22bd3ea392a2
xany <635993292@qq.com>
2018-07-16 10:43:08 +0800

    优化：短信驱动类

--- source/application/common/library/sms/Driver.php
+++ source/application/common/library/sms/Driver.php
@@ -11,8 +11,9 @@ use think\Exception;
  */
 class Driver
 {
-    private $config;    // sms 配置
-    private $engine;    // 当前存储引擎类
+    private $config;    // 配置信息
+    private $engine;    // 当前短信引擎类
+    private $engineName;    // 当前短信引擎名称
 
     /**
      * 构造方法
@@ -22,7 +23,10 @@ class Driver
      */
     public function __construct($config)
     {
+        // 配置信息
         $this->config = $config;
+        // 当前引擎名称
+        $this->engineName = $this->config['default'];
         // 实例化当前存储引擎
         $this->engine = $this->getEngineClass();
     }
@@ -35,6 +39,9 @@ class Driver
      */
     public function sendSms($msgType, $templateParams)
     {
+        if ($this->config['engine'][$this->engineName][$msgType]['is_enable'] === '0') {
+            return false;
+        }
         return $this->engine->sendSms($msgType, $templateParams);
     }
 
@@ -54,12 +61,11 @@ class Driver
      */
     private function getEngineClass()
     {
-        $engineName = $this->config['default'];
-        $classSpace = __NAMESPACE__ . '\\engine\\' . ucfirst($engineName);
+        $classSpace = __NAMESPACE__ . '\\engine\\' . ucfirst($this->engineName);
         if (!class_exists($classSpace)) {
-            throw new Exception('未找到存储引擎类: ' . $engineName);
+            throw new Exception('未找到存储引擎类: ' . $this->engineName);
         }
-        return new $classSpace($this->config['engine'][$engineName]);
+        return new $classSpace($this->config['engine'][$this->engineName]);
     }
 
 }
