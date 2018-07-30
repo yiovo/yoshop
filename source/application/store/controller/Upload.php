abf7e7eefca2e205c2c46203ca2a87a6d0d7f59f
萤火科技 <635993292@qq.com>
2018-07-30 23:07:38 +0800

    修复：后台文件库上传新文件到分组

--- source/application/store/controller/Upload.php
+++ source/application/store/controller/Upload.php
@@ -27,34 +27,36 @@ class Upload extends Controller
 
     /**
      * 图片上传接口
-     * @return \think\response\Json
+     * @param int $group_id
+     * @return array
      * @throws \think\Exception
      */
-    public function image()
+    public function image($group_id = -1)
     {
         // 实例化存储驱动
         $StorageDriver = new StorageDriver($this->config);
         // 上传图片
         if (!$StorageDriver->upload())
-            return json(['code' => 1, 'msg' => '图片上传失败' . $StorageDriver->getError()]);
+            return $this->renderError('图片上传失败 ' . $StorageDriver->getError());
         // 图片上传路径
         $fileName = $StorageDriver->getFileName();
         // 图片信息
         $fileInfo = $StorageDriver->getFileInfo();
         // 添加文件库记录
-        $uploadFile = $this->addUploadFile($fileName, $fileInfo, 'image');
+        $uploadFile = $this->addUploadFile($group_id, $fileName, $fileInfo, 'image');
         // 图片上传成功
-        return json(['code' => 1, 'msg' => '图片上传成功', 'data' => $uploadFile]);
+        return $this->renderSuccess('图片上传成功', null, $uploadFile->toArray());
     }
 
     /**
      * 添加文件库上传记录
+     * @param $group_id
      * @param $fileName
      * @param $fileInfo
      * @param $fileType
      * @return UploadFile
      */
-    private function addUploadFile($fileName, $fileInfo, $fileType)
+    private function addUploadFile($group_id, $fileName, $fileInfo, $fileType)
     {
         // 存储引擎
         $storage = $this->config['default'];
@@ -63,6 +65,7 @@ class Upload extends Controller
         // 添加文件库记录
         $model = new UploadFile;
         $model->add([
+            'group_id' => $group_id > 0 ? (int)$group_id : 0,
             'storage' => $storage,
             'file_url' => $fileUrl,
             'file_name' => $fileName,
